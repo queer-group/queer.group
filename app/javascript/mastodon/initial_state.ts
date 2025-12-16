6f37f1250307e24af5af633b375642cad9df9331
Claire <claire.github-309c@sitedethib.com>
2025-12-16 10:47:18 +0100

    Add wrapstodon to initial state and show wrapstodon sidebar item on load (#37261)

--- app/javascript/mastodon/initial_state.ts
+++ app/javascript/mastodon/initial_state.ts
@@ -2,6 +2,11 @@ import type { ApiAccountJSON } from './api_types/accounts';
 
 type InitialStateLanguage = [code: string, name: string, localName: string];
 
+interface InitialWrapstodonState {
+  year: number;
+  state: 'available' | 'generating' | 'eligible' | 'ineligible';
+}
+
 interface InitialStateMeta {
   access_token: string;
   advanced_layout?: boolean;
@@ -49,6 +54,7 @@ interface InitialStateMeta {
   terms_of_service_enabled: boolean;
   emoji_style?: string;
   expand_usernames?: boolean;
+  wrapstodon?: InitialWrapstodonState | null;
 }
 
 interface Role {
@@ -134,6 +140,7 @@ export const criticalUpdatesPending = initialState?.critical_updates_pending;
 export const statusPageUrl = getMeta('status_page_url');
 export const sso_redirect = getMeta('sso_redirect');
 export const termsOfServiceEnabled = getMeta('terms_of_service_enabled');
+export const wrapstodon = getMeta('wrapstodon');
 
 const displayNames =
   // Intl.DisplayNames can be undefined in old browsers
