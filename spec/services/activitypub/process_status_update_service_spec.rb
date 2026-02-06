e79355a4f8ef6954de2212b3c2fb546e80312e43
Claire <claire.github-309c@sitedethib.com>
2026-02-06 11:09:42 +0100

    Fix processing of object updates with duplicate hashtags (#37756)

--- spec/services/activitypub/process_status_update_service_spec.rb
+++ spec/services/activitypub/process_status_update_service_spec.rb
@@ -301,6 +301,8 @@ RSpec.describe ActivityPub::ProcessStatusUpdateService do
             { type: 'Hashtag', name: 'foo' },
             { type: 'Hashtag', name: 'bar' },
             { type: 'Hashtag', name: '#2024' },
+            { type: 'Hashtag', name: 'Foo Bar' },
+            { type: 'Hashtag', name: 'FooBar' },
           ],
         }
       end
