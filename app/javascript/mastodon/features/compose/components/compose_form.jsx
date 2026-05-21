a0f757f66e095c7d082720a397896e5cf7bb0489
diondiondion <mail@diondiondion.com>
2026-05-21 08:41:34 +0200

    Accessibility: Add visible focus outlines to main search & composer fields (#39111)

--- app/javascript/mastodon/features/compose/components/compose_form.jsx
+++ app/javascript/mastodon/features/compose/components/compose_form.jsx
@@ -85,10 +85,6 @@ class ComposeForm extends ImmutablePureComponent {
     autoFocus: false,
   };
 
-  state = {
-    highlighted: false,
-  };
-
   constructor(props) {
     super(props);
     this.textareaRef = createRef(null);
@@ -227,8 +223,6 @@ class ComposeForm extends ImmutablePureComponent {
       Promise.resolve().then(() => {
         this.textareaRef.current.setSelectionRange(selectionStart, selectionEnd);
         this.textareaRef.current.focus();
-        this.setState({ highlighted: true });
-        this.timeout = setTimeout(() => this.setState({ highlighted: false }), 700);
       }).catch(console.error);
     } else if(prevProps.isSubmitting && !this.props.isSubmitting) {
       this.textareaRef.current.focus();
@@ -259,7 +253,6 @@ class ComposeForm extends ImmutablePureComponent {
 
   render () {
     const { intl, onPaste, onDrop, autoFocus, withoutNavigation, maxChars, isSubmitting } = this.props;
-    const { highlighted } = this.state;
     const disabled = this.props.quoteOfLocalOnly || this.props.isEditing;
 
     return (
@@ -268,7 +261,7 @@ class ComposeForm extends ImmutablePureComponent {
         {!withoutNavigation && <NavigationBar />}
         <Warning />
 
-        <div className={classNames('compose-form__highlightable', { active: highlighted })} ref={this.setRef}>
+        <div className='compose-form__highlightable' ref={this.setRef}>
           <EditIndicator />
 
           <div className='compose-form__dropdowns'>
