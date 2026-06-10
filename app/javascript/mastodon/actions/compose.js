fad7477e750046ea16339eecb69aa863f3ff37c0
diondiondion <mail@diondiondion.com>
2026-06-10 16:50:49 +0200

    [Accessibility] Navigation focus handling polish (#39358)

--- app/javascript/mastodon/actions/compose.js
+++ app/javascript/mastodon/actions/compose.js
@@ -94,7 +94,7 @@ const messages = defineMessages({
 
 export const ensureComposeIsVisible = (getState) => {
   if (!getState().getIn(['compose', 'mounted'])) {
-    browserHistory.push('/publish');
+    browserHistory.push('/publish', { focusTarget: false });
   }
 };
 
@@ -289,6 +289,16 @@ export function submitCompose(successCallback) {
         insertIfOnline('public');
         insertIfOnline(`account:${response.data.account.id}`);
       }
+
+      dispatch(showAlert({
+        message: statusId === null ? messages.published : messages.saved,
+        action: messages.open,
+        dismissAfter: 10000,
+        onClick: () => browserHistory.push(
+          `/@${response.data.account.username}/${response.data.id}`,
+          { focusTarget: 'detailed-status' }
+        ),
+      }));
     }).catch(function (error) {
       dispatch(submitComposeFail(error));
     });
